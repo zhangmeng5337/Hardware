ca9a635f633c5cb1977403a3fba4b2d668af9f16
zhangmeng5337 <zhangmeng5337@126.com>
2019-09-09 20:50:46 +0800

    lora

---
+++ sensor/code/GSM/Include/GSM_BSP.h
@@ -0,0 +1,11 @@
+#ifndef GSM_BSP
+#define GSM_BSP
+
+#define ON  1
+#define OFF 0
+#define MAX_STEP   5
+#define buffer_size  256
+#define Max_Network_thres 30
+void modulePowerOn(unsigned char pwr_ctrl);
+void Send_Comm(unsigned char* comm,unsigned short len);
+#endif
