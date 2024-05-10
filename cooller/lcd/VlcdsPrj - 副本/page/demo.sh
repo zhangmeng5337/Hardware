f58f2aa481ddaca71b457007bbd5d8165ad10345
zhangmeng5337 <zhangmeng5337@126.com>
2024-05-10 16:48:15 +0800

    lcd

--- cooller/lcd/VlcdsPrj - 副本/page/demo.sh
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" standalone="no"?>
-<!DOCTYPE vlcd PUBLIC "-//W3C//DTD VLCD 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/vlcd10.dtd">
-<vlcd width="480" height="272">
-<desc>Vlcd picture</desc>
-</vlcd>
